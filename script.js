9ede4a1909840812be9eb79966c314363e1002f8
Dan Monolith <monolithicmail@gmail.com>
2022-10-13 13:48:41 +0500

    Clean up code, make it more concise

--- script.js
+++ script.js
@@ -3,54 +3,53 @@ function rpsRound(getComputerSelection, getPlayerSelection) {
     function getComputerChoice () {
         randomNum = Math.floor(Math.random() * 9) + 1;
         
-        if (randomNum >= 1 && randomNum <= 3) {
-            return ("rock");
-        } else if (randomNum >= 4 && randomNum <= 6) {
-            return ("paper");
-        } else {
-            return ("scissors");
-        }
+        return(randomNum >= 1 && randomNum <= 3) ? ("rock") :
+            (randomNum >= 4 && randomNum <= 6) ? ("paper") :
+            ("scissors");
     }
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
     
     function getPlayerChoice() {
         let playerInput = prompt("Rock, Paper or Scissors?");
-    
-        playerInput = playerInput.toLowerCase();
-        if (playerInput === "rock") {
-            return ("rock");
-        } else if (playerInput === "paper") {
-            return ("paper");
-        } else if (playerInput === "scissors") {
-            return ("scissors");
-        } else {
-            alert("That's not Rock Paper Scissors! Please reload the page");
-        }        
+        
+        return (playerInput === "rock") ? ("rock") :
+            (playerInput === "paper") ? ("paper") :
+            (playerInput === "scissors") ? ("scissors") :
+            alert("That's not Rock Paper Scissors!, You made a typo");  
     }
     let playerSelection = getPlayerChoice();
     console.log(playerSelection);
    
     let rpsValue = computerSelection.concat(playerSelection);
 
-    if (rpsValue === "rockrock") {
-        return (score = 0), alert("Draw");
-    } else if (rpsValue === "rockpaper") {
-        return (score = 1), alert("You win! Paper beats rock");
-    } else if (rpsValue === "rockscissors") {
-        return (score = -1), alert("You lose! Rock beats scissors");
-    } else if (rpsValue === "paperpaper") {
-        return (score = 0), alert("Draw");
-    } else if (rpsValue === "paperscissors") {
-        return (score = 1), alert("You win! Scissors beats paper");
-    } else if (rpsValue === "paperrock") {
-        return (score = -1), alert("You lose! Paper beats rock");
-    } else if (rpsValue === "scissorsscissors") {
-        return (score = 0), alert("Draw");
-    } else if (rpsValue === "scissorsrock") {
-        return (score = 1), alert("You win! Rock beats scissors");
-    } else { 
-        return(score = -1), alert("You lose! Scissors beats paper");
+    switch (rpsValue) {
+        case "rockrock":
+            return (score = 0), alert("Draw");
+            break;
+        case "rockpaper":
+            return (score = 1), alert("You win! Paper beats rock");
+            break;
+        case "rockscissors":
+            return (score = -1), alert("You lose! Rock beats scissors");
+            break;
+        case "paperpaper":
+            return (score = 0), alert("Draw");
+            break;
+        case "paperscissors":
+            return (score = 1), alert("You win! Scissors beats paper");
+            break;
+        case "paperrock":
+            return (score = -1), alert("You lose! Paper beats rock");
+            break;
+        case "scissorsscissors":
+            return (score = 0), alert("Draw");
+            break;
+        case "scissorsrock":
+            return (score = 1), alert("You win! Rock beats scissors");
+            break;
+        default:
+            return(score = -1), alert("You lose! Scissors beats paper");        
     }
 }
 let score;
@@ -62,14 +61,10 @@ function game() {
         rpsRound();
         finalScore += score;
     }
-    if (finalScore > 0) {
-        alert("You win the game! Good job old boy!");
-    } else if (finalScore < 0) {
-        alert("Sorry, you lost the match old bean");
-    } else {
-        alert("Wow, it was a draw!");
-    }
-  
+
+    let message = (finalScore > 0) ? alert("You win the game! Good job old boy!"):
+        (finalScore < 0) ? alert("Sorry, you lost the match old bean") :
+        alert("Wow, it was a draw!");  
 }
 
 if (confirm("Wanna play Rock Paper Scissors?")) {
@@ -77,5 +72,3 @@ if (confirm("Wanna play Rock Paper Scissors?")) {
   } else {
     alert("Fine, do whatever you want!");
   }
-
-  //now to tidy up and make it look good.
