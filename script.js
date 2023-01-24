70d1c82a8a876e2ce2c2f1f290251bfc41bbc1fe
Dan Monolith <monolithicmail@gmail.com>
2023-01-24 17:02:42 +0500

    Try to implement UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function rpsRound(getComputerSelection, getPlayerSelection) {
+function rpsRound(getComputerSelection, PlayerSelection) {
    
     function getComputerChoice () {
         randomNum = Math.floor(Math.random() * 9) + 1;
@@ -10,19 +10,25 @@ function rpsRound(getComputerSelection, getPlayerSelection) {
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
     
-    function getPlayerChoice() {
-        let playerInput = prompt("Rock, Paper or Scissors?");
-        
-        return (playerInput === "rock") ? ("rock") :
-            (playerInput === "paper") ? ("paper") :
-            (playerInput === "scissors") ? ("scissors") :
-            alert("That's not Rock Paper Scissors!, You made a typo");  
-    }
-    let playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    //function getPlayerChoice() {
+    //    let playerInput = prompt("Rock, Paper or Scissors?");
+    //    
+    //    //need to input a empty input catcher if player enters nothing and clicks OK
+    //    //or the players makes a typo, currently it just runs to the last case in rpsValue
+
+    //    return (playerInput === "rock") ? ("rock") :
+    //        (playerInput === "paper") ? ("paper") :
+    //        (playerInput === "scissors") ? ("scissors") :
+    //        (playerInput === "") ? ("blank") :
+    //        alert("That's not Rock Paper Scissors!, You made a typo");  
+    //}
+    //let playerSelection = getPlayerChoice();
+    //console.log(playerSelection);
    
+
     let rpsValue = computerSelection.concat(playerSelection);
 
+    //maybe I can use backticks to improve readability and make code less verbose
     switch (rpsValue) {
         case "rockrock":
             return (score = 0), alert("Draw");
@@ -48,8 +54,11 @@ function rpsRound(getComputerSelection, getPlayerSelection) {
         case "scissorsrock":
             return (score = 1), alert("You win! Rock beats scissors");
             break;
-        default:
-            return(score = -1), alert("You lose! Scissors beats paper");        
+        case "scissorspaper":
+            return(score = -1), alert("You lose! Scissors beats paper");
+            break;
+        default :
+            alert("Are you OK?");        
     }
 }
 let score;
@@ -67,8 +76,41 @@ function game() {
         alert("Wow, it was a draw!");  
 }
 
-if (confirm("Wanna play Rock Paper Scissors?")) {
-    game();
-  } else {
-    alert("Fine, do whatever you want!");
-  }
+//if (confirm("Wanna play Rock Paper Scissors?")) {
+//    game();
+//  } else {
+//    alert("Fine, do whatever you want!");
+//  }
+
+const buttons = document.querySelectorAll ('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;
+        console.log(playerSelection);
+        rpsRound();
+    });
+});
+
+const bodyClass = document.querySelector('body');
+
+const playDiv = document.createElement('div');
+playDiv.classList.add('playDiv');
+bodyClass.appendChild(playDiv);
+
+const playButton = document.createElement('button',);
+playButton.classList.add('playButton');
+playButton.textContent = 'Play';
+playButton.style.marginTop = "10px";
+playDiv.appendChild(playButton); 
+playButton.onclick = () => game();
+
+
+
+
+
+
+
+
+
+    
